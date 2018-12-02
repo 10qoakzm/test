3f3dae52fd121d92d6ce0a2b0bcb8c68f235d22a
10qoakzm <45536513+10qoakzm@users.noreply.github.com>
2018-12-03 00:32:57 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 23:54:21 2018
+
+@author: yyy
+"""
+
+a = [1,2,3]
+b = [4,5,6] 
+
+for c in a:
+    for d in b:
+        num = c * d
+        print(num)
+        
